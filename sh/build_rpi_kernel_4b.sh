00b7271025ed3d2893e187893f0cba49684ba90c
namyuri@gmail.com <namyuri@gmail.com>
2021-02-12 01:56:24 +0000

    4B config

---
+++ sh/build_rpi_kernel_4b.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "configure build output path"
+
+KERNEL_TOP_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+OUTPUT="$KERNEL_TOP_PATH/out"
+echo "$OUTPUT"
+
+KERNEL=kernel7l
+BUILD_LOG="$KERNEL_TOP_PATH/rpi_build_log.txt"
+
+echo "move kernel source"
+cd linux
+
+echo "make defconfig"
+make O=$OUTPUT bcm2711_defconfig
+
+echo "kernel build"
+make O=$OUTPUT zImage modules dtbs -j4 2>&1 | tee $BUILD_LOG
+
